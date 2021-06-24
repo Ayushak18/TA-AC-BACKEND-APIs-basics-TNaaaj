6ab41de019397311bc46d80519e44a28bf3c8250
Ayushak18 <aayushkamboj7@gmail.com>
2021-06-25 02:21:38 +0530

    Filters

--- block-BNaaei/Country/routes/country.js
+++ block-BNaaei/Country/routes/country.js
@@ -6,11 +6,42 @@ let Country = require('../model/country');
 
 // All countries
 router.get('/', function (req, res, next) {
-  res.status(200).json({ country: 'India' });
+  Country.find({}, (error, countries) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json(countries);
+    }
+  });
 });
 
+// Create country
 router.post('/', (req, res, next) => {
   Country.create(req.body, (error, country) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json(country);
+    }
+  });
+});
+
+// Update Country
+router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Country.findByIdAndUpdate(id, req.body, { new: true }, (error, country) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json(country);
+    }
+  });
+});
+
+// Delete Country
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Country.findByIdAndDelete(id, (error, country) => {
     if (error) {
       next(error);
     } else {
